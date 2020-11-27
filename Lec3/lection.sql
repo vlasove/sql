6ebaf47a08a61453c87d9e6a3357431f2ab19a7a
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-11-27 17:30:34 +0300

    Duplicate descending part

--- Lec3/lection.sql
+++ Lec3/lection.sql
@@ -1,1 +1,21 @@
 /* Queries alphabet */ 
+use bank;
+
+/* Simple selection */
+SELECT emp_id, fname, lname 
+FROM employee;
+
+/* SELECT inspect */ 
+SELECT * FROM department;
+
+SELECT name FROM department;
+
+/* Literal queries */ 
+SELECT emp_id, 'ACTIVE' activator, emp_id * 3.14, UPPER(lname) FROM employee;
+
+/* Config Info selection */
+SELECT VERSION(), USER(), DATABASE();
+
+/* Duplicate descending */ 
+
+SELECT DISTINCT cust_id FROM account;
